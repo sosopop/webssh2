3dbd1090fe6c33b697d2b6aa8f4f551bd2ef7786
mengchao <mengchao@cnnho.com>
2021-02-18 18:34:02 +0800

    增加代理的支持http://localhost:2222/ssh/host/127.0.0.1?port=22&user=mengchao&pass=ipconfig&proxy=cabbage1102.oicp.net&proxy-port=11111&proxy-user=aaa&proxy-pass=12345678

--- app/server/util.js
+++ app/server/util.js
@@ -1,44 +1,55 @@
-'use strict'
+"use strict";
+
+const { json } = require("express");
+
 /* jshint esversion: 6, asi: true, node: true */
 // util.js
 
 // private
-require('colors') // allow for color property extensions in log messages
-var debug = require('debug')('WebSSH2')
-var Auth = require('basic-auth')
+require("colors"); // allow for color property extensions in log messages
+var debug = require("debug")("WebSSH2");
+//var Auth = require("basic-auth");
 
-const defaultCredentials = { username: null, password: null, privatekey: null }
+const defaultCredentials = { username: null, password: null, privatekey: null };
 
 exports.setDefaultCredentials = function (username, password, privatekey) {
-  defaultCredentials.username = username
-  defaultCredentials.password = password
-  defaultCredentials.privatekey = privatekey
-}
+  defaultCredentials.username = username;
+  defaultCredentials.password = password;
+  defaultCredentials.privatekey = privatekey;
+};
 
-exports.basicAuth = function basicAuth (req, res, next) {
-  var myAuth = Auth(req)
-  if (myAuth && myAuth.pass !== '') {
-    req.session.username = myAuth.name
-    req.session.userpassword = myAuth.pass
-    debug('myAuth.name: ' + myAuth.name.yellow.bold.underline +
-      ' and password ' + ((myAuth.pass) ? 'exists'.yellow.bold.underline
-      : 'is blank'.underline.red.bold))
-  } else {
-    req.session.username = defaultCredentials.username
-    req.session.userpassword = defaultCredentials.password
-    req.session.privatekey = defaultCredentials.privatekey
+exports.basicAuth = function basicAuth(req, res, next) {
+  // var myAuth = Auth(req)
+  // if (myAuth && myAuth.pass !== '') {
+  //   req.session.username = myAuth.name
+  //   req.session.userpassword = myAuth.pass
+  //   debug('myAuth.name: ' + myAuth.name.yellow.bold.underline +
+  //     ' and password ' + ((myAuth.pass) ? 'exists'.yellow.bold.underline
+  //     : 'is blank'.underline.red.bold))
+  // } else {
+  //   req.session.username = defaultCredentials.username
+  //   req.session.userpassword = defaultCredentials.password
+  //   req.session.privatekey = defaultCredentials.privatekey
+  // }
+  if (req.query.user && req.query.pass) {
+    req.session.username = req.query.user;
+    req.session.userpassword = req.query.pass;
+    req.session.proxy = req.query.proxy;
+    req.session.proxyPort = req.query["proxy-port"];
+    req.session.proxyUser = req.query["proxy-user"];
+    req.session.proxyPass = req.query["proxy-pass"];
   }
-  if ((!req.session.userpassword) && (!req.session.privatekey)) {
-    res.statusCode = 401
-    debug('basicAuth credential request (401)')
-    res.setHeader('WWW-Authenticate', 'Basic realm="WebSSH"')
-    res.end('Username and password required for web SSH service.')
-    return
-  }
-  next()
-}
+  // if (!req.session.userpassword && !req.session.privatekey) {
+  //   res.statusCode = 401;
+  //   debug("basicAuth credential request (401)");
+  //   //res.setHeader("WWW-Authenticate", 'Basic realm="WebSSH"');
+  //   res.end("Username and password required for web SSH service.");
+  //   return;
+  // }
+  next();
+};
 
 // takes a string, makes it boolean (true if the string is true, false otherwise)
-exports.parseBool = function parseBool (str) {
-  return (str.toLowerCase() === 'true')
-}
+exports.parseBool = function parseBool(str) {
+  return str.toLowerCase() === "true";
+};
